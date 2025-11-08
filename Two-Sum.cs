ce7e2852e82629628da64b37c68cd8d1ae2d6338
mateushrc <mat3us.hrc@gmail.com>
2025-11-08 12:28:26 -0300

    first commit

---
+++ Two-Sum.cs
@@ -0,0 +1,18 @@
+//My solution in C#
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if ((nums[j] + nums[i]) == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+        return [];
+    }
+}
